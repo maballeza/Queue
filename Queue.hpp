78fb44be23b314132cb97abbadb94470a30bc262
Miguel Balleza <maballeza@uh.edu>
2021-04-20 15:06:30 -0400

    Node member 'data' changed from T* to T; Enqueue() now returns a pointer to Node::data; both [En-/De-]queue() updated for clarity

--- Queue.hpp
+++ Queue.hpp
@@ -7,110 +7,95 @@ public:
     /**
      * The condition 'head' == 'tail' is used to signal an empty queue.
      */
-    Queue() : head(Allocate()), tail(head) {}
+    Queue() : tail(nullptr) { head = tail; }
     ~Queue();
 
-    void Enqueue(T&& obj);
-    T* Dequeue();
+    T* Enqueue(T&& obj);
+    T Dequeue();
 
 private:
     struct Node
     {
-        Node() : data(nullptr), next(nullptr) {}
-        Node(T* dat) : data(dat), next(nullptr) {}
-        Node(Node* N) : data(N->data), next(N->next) {}
-        ~Node() { if (data) Deallocate(data); next = nullptr; }
-        T* data;
+        Node(T dat) : data(dat), next(nullptr) {}
+        ~Node() {}
+        T data;
         Node* next;
     };
 
-    Node* Allocate(T* obj = nullptr) noexcept;
-    static void Deallocate(T* obj) noexcept;
-    static void Deallocate(Node* n) noexcept;
+    Node* Allocate(T&& obj) noexcept;
+    void DeallocateList(Node* n);
     
     Node* head;  // Value-storing, initialized as nullptr
     Node* tail;  // Always nullptr
 };
 
 template<typename T>
-inline Queue<T>::~Queue()
-{
-    if (tail != head)
-        Deallocate(tail);
-    Deallocate(head);
-}
-
-template<typename T>
-inline void Queue<T>::Enqueue(T&& obj) {
-    if (head == tail) {
-        Deallocate(head);
-        head = Allocate(&obj);
-        tail = Allocate();
-        head->next = tail;
-        return;
+inline Queue<T>::~Queue() {
+    if (head != tail) { // Checks for an empty list.
+        DeallocateList(head);
     }
-    Node* adj = head;
-    while (adj->next != tail) {
-        adj = adj->next;
-    }
-    adj->next = Allocate(&obj);
-    adj->next->next = tail;
 }
 
 template<typename T>
-inline T* Queue<T>::Dequeue() {
-    if (head == tail)
-        return nullptr;
-    T ret = *head->data;
-    Node* temp = head;
-    head = temp->next;
-    head->next = temp->next->next;
-    Deallocate(temp);
-    
-    return &ret;
-}
-
-template<typename T>
-typename Queue<T>::Node* Queue<T>::Allocate(T* obj) noexcept {
+typename Queue<T>::Node* Queue<T>::Allocate(T&& obj) noexcept {
     try {
-        if (obj != nullptr) 
-            obj = new T{ *obj };
-    }
-    catch (std::exception& e) {
-        std::cerr << e.what() << std::endl
-                  << "Object allocation failure." << std::endl;
-    }
-
-    try {
-        Node* ret = new Node{ obj };
+        Node* ret = new Node{ std::move(obj) };
         return ret;
     }
     catch (std::exception& e) {
         std::cerr << e.what() << std::endl
-                  << "Node allocation failure." << std::endl;
+                  << "Object allocation failure." << std::endl;
+        return nullptr;
     }
 }
 
 template<typename T>
-void Queue<T>::Deallocate(T* obj) noexcept {
-    try {
-        delete obj;
-        obj = nullptr;
+void Queue<T>::DeallocateList(Node* n) {
+    if (n->next != nullptr) {
+        DeallocateList(n->next);
     }
-    catch (std::exception& e) {
-        std::cerr << e.what() << std::endl
-                  << "Object deallocation failure." << std::endl;
+    delete n;
+}
+
+template<typename T>
+T* Queue<T>::Enqueue(T&& obj) {
+    Node* adj = Allocate(std::move(obj));
+    if (adj) {
+        if (!head) { // Queue initialization: tail == nullptr.
+            head = adj;
+            head->next = tail;
+            return &head->data;
+        }
+        else if (Node* found = head->next) { // Search for the node preceding the tail.
+            while (found->next != tail) {
+                found = found->next;
+            }
+            found->next = adj;
+        }
+        else { // Inductive step.
+            head->next = adj;
+        }
+        adj->next = tail;
+        return &adj->data;
+    }
+    else {
+        return nullptr;
     }
 }
 
 template<typename T>
-void Queue<T>::Deallocate(Node* n) noexcept {
-    try {
-        delete n;
-        n = nullptr;
-    }
-    catch (std::exception& e) {
-        std::cerr << e.what() << std::endl
-                  << "Node deallocation failure." << std::endl;
+T Queue<T>::Dequeue() {
+    if (head != tail) {
+        T ret = head->data;
+        Node* temp = head;
+        if (head->next) {
+            head = temp->next;
+            head->next = temp->next->next;
+        }
+        else {
+            head = tail;
+        }
+        delete temp;    // Assigning nullptr to temp->next is unnecessary in this context.
+        return std::move(ret);
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
+#include <string>
+#include <array>
 #include "Queue.hpp"
 
 int main()
 {
-
-    Queue<int> q;
+    Queue<char> qc;
+    Queue<int> qi;
     Queue<float> qf;
-    
-    int size = 100;
-    float f = 0;
-    while (size) {
-        f = f + 0.1f;
-        q.Enqueue(size--);
-        qf.Enqueue(static_cast<float&&>(f));
+    Queue<std::string> qs;
+
+    const int size = 10;
+    std::array<char, size> charA;
+    std::array<int, size> intA;
+    std::array<float, size> floatA;
+    std::array<std::string, size> strA;
+
+    for (int sz = 0;  sz < size; sz++) {
+        char ch = sz;
+        float f = sz;  // Widening.
+        std::string name{ std::to_string(ch) };
+
+        charA[sz] = *qc.Enqueue(std::move(ch));
+        intA[sz] = *qi.Enqueue(std::move(sz));
+        floatA[sz] = *qf.Enqueue(std::move(f));
+        strA[sz] = *qs.Enqueue(std::move(name));
     }
 
-    while (auto v = q.Dequeue()) {
-        std::cout << *v << std::endl;
-    }
-    while (auto v = qf.Dequeue()) {
-        std::cout << *v << std::endl;
+    for (int sz = 0; sz < size; sz++) {
+        if (charA[sz] != qc.Dequeue()) throw;
+        if (intA[sz] != qi.Dequeue()) throw;
+        if (floatA[sz] != qf.Dequeue()) throw;
+        if (strA[sz] != qs.Dequeue()) throw;
     }
 }
